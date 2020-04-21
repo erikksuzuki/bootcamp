e56e02691b6d75161aec8cffd01114b218230546
erikksuzuki <erikksuzuki@gmail.com>
2020-04-21 18:48:53 +0800

    Header and Footer

--- Front-End_Web_Development_with_React/confusion/src/components/MainComponent.js
+++ Front-End_Web_Development_with_React/confusion/src/components/MainComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import Dishdetails from './DishdetailComponent'
 import { DISHES } from '../shared/dishes';
 
@@ -23,15 +25,12 @@ class Main extends Component {
     render() {
         return (
         <div className="App">
-            <Navbar dark color="primary">
-            <div className="container">
-                <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-            </div>
-            </Navbar>
+            <Header />
             <Menu dishes={this.state.dishes}
                 onClick={(dishId) => this.onDishSelect(dishId)} />
             <Dishdetails key={this.state.dishes.id}
                 currentdish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish )[0]} />
+            <Footer />
         </div>
         )
     }
