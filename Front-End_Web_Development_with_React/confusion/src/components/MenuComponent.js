af1be58656b992b9a66e0bf8c25f13187da9ccf4
erikksuzuki <erikksuzuki@gmail.com>
2020-04-27 16:49:16 +0800

    Redux Thunk

--- Front-End_Web_Development_with_React/confusion/src/components/MenuComponent.js
+++ Front-End_Web_Development_with_React/confusion/src/components/MenuComponent.js
@@ -3,6 +3,9 @@ import { Card, CardImg, CardImgOverlay,
     CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import Dishdetails from "./DishdetailComponent";
+import { Loading } from './LoadingComponent';
+
+
 
 function RenderMenuItem ({dish, onClick}) {
     return (
@@ -18,31 +21,53 @@ function RenderMenuItem ({dish, onClick}) {
 }
 
 const Menu = (props) => {
-  const menu = props.dishes.map((dish) => {
-    return (
-      <div key={dish.id} className="col-12 col-md-5 m-1">
-        <RenderMenuItem dish={dish} onClick={props.onClick} />
-      </div>
+    const menu = props.dishes.dishes.map((dish) => {
+        return (
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+            <RenderMenuItem dish={dish} onClick={props.onClick} />
+        </div>
+        );
+    });
+         
+    if (props.dishes.isLoading) {
+    return(
+        <div className="container">
+            <div className="row">            
+                <Loading />
+            </div>
+        </div>
     );
-  });
-
-  return (
-    <div className="container">
-        <div className="row">
-            <Breadcrumb>
-                <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                <BreadcrumbItem active>Menu</BreadcrumbItem>
-            </Breadcrumb>
-            <div className="col-12">
-                <h3>Menu</h3>
-                <hr />
-            </div>                
+    }
+    else if (props.dishes.errMess) {
+    return(
+        <div className="container">
+            <div className="row"> 
+                <div className="col-12">
+                    <h4>{props.dishes.errMess}</h4>
+                </div>
+            </div>
         </div>
-        <div className="row">
-            {menu}
-        </div>
-    </div>
-);
+    );
+    }
+    else {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    {menu}
+                </div>
+            </div>
+        );
+    }
 };
 
 export default Menu;
