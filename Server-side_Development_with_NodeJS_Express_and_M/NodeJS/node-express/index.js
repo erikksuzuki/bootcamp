2f90e472af8158f322a68e3f375693c67c6ad8e6
erikksuzuki <erikksuzuki@gmail.com>
2020-05-01 20:20:20 +0800

    Express Serve Static Files

--- Server-side_Development_with_NodeJS_Express_and_M/NodeJS/node-express/index.js
+++ Server-side_Development_with_NodeJS_Express_and_M/NodeJS/node-express/index.js
@@ -1,10 +1,14 @@
-const express = require("express"),
-  http = require("http");
+const express = require("express");
+const http = require("http");
+const morgan = require("morgan");
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
+app.use(morgan("dev"));
+
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res, next) => {
   console.log(req.headers);
@@ -18,4 +22,3 @@ const server = http.createServer(app);
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-n;
